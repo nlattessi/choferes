9b41b237f261a7d2ac9d2eb0d2f508fac330d84e
Nahuel Lattessi <n_lattessi@hotmail.com>
2016-01-17 16:16:32 -0300

    remueve pre y cola cuil, agrega cuil, en chofer

--- src/ChoferesBundle/Resources/public/js/cuilmask.js
+++ src/ChoferesBundle/Resources/public/js/cuilmask.js
@@ -15,4 +15,12 @@
          "clearMaskOnLostFocus": false,
        }
      );
+     $("#choferesbundle_chofer_cuil")
+      .inputmask(
+        "99-99999999-9",
+        {
+          "removeMaskOnSubmit": true,
+          "clearMaskOnLostFocus": false,
+        }
+      );
 })(window, document, jQuery);

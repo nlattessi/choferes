58630ebb09ffabd013cdf26953e83eaf2a5d4ba8
Nahuel Lattessi <n_lattessi@hotmail.com>
2016-01-18 16:42:01 -0300

    query para ver choferes que les cargaron curso basico pero no cambio su tieneCursoBasico a true

---
+++ scripts/query_ver_choferes_curso_basico.sql
@@ -0,0 +1,10 @@
+SELECT 
+chofer.id as choferId, chofer.nombre, chofer.apellido, chofer.dni,
+chofer.tiene_curso_basico as tieneCursoBasico,
+chofer_curso.id as choferCursoId, chofer_curso.aprobado as aprobado, chofer_curso.pagado,
+chofer_curso.documentacion, curso.id as cursoId, curso.fecha_fin as fechaFin
+FROM chofer
+LEFT JOIN chofer_curso ON chofer_curso.chofer_id = chofer.id
+LEFT JOIN curso ON chofer_curso.curso_id = curso.id
+WHERE curso.tipoCurso_id = 1 AND chofer.tiene_curso_basico = 0 AND chofer_curso.aprobado > 3 AND chofer_curso.pagado = 1
+ORDER BY curso.fecha_fin DESC;

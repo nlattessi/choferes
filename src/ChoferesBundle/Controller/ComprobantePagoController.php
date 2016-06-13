dad19ae798de6a7df88213ff30331162b5f36a86
Nahuel Lattessi <n_lattessi@hotmail.com>
2016-06-13 00:33:19 -0300

    delete and update comprobante pago (edit WIP)

--- src/ChoferesBundle/Controller/ComprobantePagoController.php
+++ src/ChoferesBundle/Controller/ComprobantePagoController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ComprobantePagoController extends Controller
 {
+    // $id => curso id
     public function createAction(Request $request, $id)
     {
         $em = $this->getDoctrine()->getManager();
@@ -45,5 +46,101 @@ class ComprobantePagoController extends Controller
         }
 
         return $this->redirect($this->generateUrl('curso_show', ['id' => $id]));
+    }
+
+    public function editAction($idCurso, $idComprobantePago)
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        $curso = $em->getRepository('ChoferesBundle:Curso')->find($idCurso);
+        if (! $curso) {
+            throw $this->createNotFoundException('Unable to find Curso entity.');
+        }
+
+        $comprobantePago = $em->getRepository('ChoferesBundle:ComprobantePago')->find($idComprobantePago);
+        if (! $comprobantePago) {
+            throw $this->createNotFoundException('Unable to find ComprobantePago entity.');
+        }
+
+        $editForm = $this->createForm(new ComprobantePagoType(), $comprobantePago);
+
+        return $this->render('ChoferesBundle:ComprobantePago:edit.html.twig', [
+            'curso'           => $curso,
+            'comprobantePago' => $comprobantePago,
+            'edit_form'       => $editForm->createView(),
+        ]);
+    }
+
+    public function updateAction(Request $request, $idCurso, $idComprobantePago)
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        $curso = $em->getRepository('ChoferesBundle:Curso')->find($idCurso);
+        if (! $curso) {
+            throw $this->createNotFoundException('Unable to find Curso entity.');
+        }
+
+        $comprobantePago = $em->getRepository('ChoferesBundle:ComprobantePago')->find($idComprobantePago);
+        if (! $comprobantePago) {
+            throw $this->createNotFoundException('Unable to find ComprobantePago entity.');
+        }
+
+        $editForm = $this->createForm(new ComprobantePagoType(), $comprobantePago);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($comprobantePago);
+            $em->flush();    
+            
+            $this->get('session')->getFlashBag()->add('success', 'Comprobante de Pago actualizado.');
+
+            
+
+        } else {
+            foreach ($comprobantePagoForm->getErrors() as $error) {
+                $this->get('session')->getFlashBag()->add(
+                        'error',
+                        'Comprobante Pago :: ' . $error->getMessage()
+                    );
+            }
+
+            $errors = $this->get('validator')->validate($comprobantePago);
+            foreach($errors as $error) {
+                $this->get('session')->getFlashBag()->add('error', 'Comprobante Pago ::: ' . $error->getMessage());
+            }
+        }
+
+        // if ($editForm->isValid()) {
+        //     $em->persist($entity);
+        //     $em->flush();
+        //     $this->get('session')->getFlashBag()->add('success', 'flash.update.success');
+
+        //     return $this->redirect($this->generateUrl('docente_edit', array('id' => $id)));
+        // } else {
+        //     $this->get('session')->getFlashBag()->add('error', 'flash.update.error');
+        // }
+
+        return $this->render('ChoferesBundle:ComprobantePago:edit.html.twig', [
+            'curso'           => $curso,
+            'comprobantePago' => $comprobantePago,
+            'edit_form'       => $editForm->createView(),
+        ]);
+    }
+
+    public function deleteAction(Request $request, $idCurso, $idComprobantePago)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $comprobantePago = $em->getRepository('ChoferesBundle:ComprobantePago')->find($idComprobantePago);
+
+        if (! $comprobantePago) {
+            throw $this->createNotFoundException('Unable to find ComprobantePago entity.');
+        }
+
+        $em->remove($comprobantePago);
+        $em->flush();
+
+        $this->get('session')->getFlashBag()->add('success', 'Comprobante eliminado satisfactoriamente.');
+
+        return $this->redirect($this->generateUrl('curso_show', ['id' => $idCurso]));
     }
 }

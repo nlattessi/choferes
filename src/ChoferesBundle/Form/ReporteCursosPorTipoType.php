cbce8447a0a0938472a9a7e0f37845f218aeaf8a
Nahuel Lattessi <n_lattessi@hotmail.com>
2016-06-26 17:55:41 -0300

    remove comment, added blank line

--- src/ChoferesBundle/Form/ReporteCursosPorTipoType.php
+++ src/ChoferesBundle/Form/ReporteCursosPorTipoType.php
@@ -10,12 +10,6 @@ class ReporteCursosPorTipoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            // ->add('tipoCurso', 'entity', [
-            //     'class' => 'ChoferesBundle:TipoCurso',
-            //     'property' => 'nombre',
-            //     'mapped' => 'false',
-            //     'required' => true
-            // ])
             ->add('fechaInicioDesde', 'text', ['mapped' => false, 'required' => true])
             ->add('fechaInicioHasta', 'text', ['mapped' => false, 'required' => true])
             ->add('reset', 'reset', ['label' => 'Limpiar '])

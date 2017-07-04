0ac4d6021ddbe489a0352a9d2e987f762cb97dd8
Nahuel Lattessi <n_lattessi@hotmail.com>
2017-07-04 18:35:14 -0300

    con ambos metodos por las dudas

--- src/ChoferesBundle/Controller/AuditoriaController.php
+++ src/ChoferesBundle/Controller/AuditoriaController.php
@@ -10,6 +10,7 @@ use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\View\TwitterBootstrapView;
 
 use ChoferesBundle\Entity\Auditoria;
+use ChoferesBundle\Entity\EstadoAuditoria;
 
 
 /**
@@ -24,8 +25,62 @@ class AuditoriaController extends Controller
      */
     public function indexAction()
     {
+        $auditorias = $this->getDoctrine()->getRepository('ChoferesBundle:Auditoria')->findAll();
 
-        return $this->render('ChoferesBundle:Auditoria:index.html.twig', array(
-        ));
+        return $this->render('ChoferesBundle:Auditoria:index.html.twig', [
+            'auditorias' => $auditorias,
+            'pagetitle' => 'Auditorias',
+            'css_active' => 'auditorias',
+        ]);
+    }
+
+    public function indexAuditoriasActivasAction()
+    {
+        $auditorias = $this->getDoctrine()->getRepository('ChoferesBundle:Auditoria')
+            ->findByEstado(EstadoAuditoria::ID_ACTIVA);
+
+        return $this->render('ChoferesBundle:Auditoria:index.html.twig', [
+            'auditorias' => $auditorias,
+            'pagetitle' => 'Auditorias Activas',
+            'css_active' => 'auditoria_activas',
+        ]);
+    }
+
+    public function indexAuditoriasIntermedioAction()
+    {
+        $auditorias = $this->getDoctrine()->getRepository('ChoferesBundle:Auditoria')
+            ->findByEstado(EstadoAuditoria::ID_INTERMEDIO);
+
+        return $this->render('ChoferesBundle:Auditoria:index.html.twig', [
+            'auditorias' => $auditorias,
+            'pagetitle' => 'Auditorias en estado Intermedio',
+            'css_active' => 'auditoria_intermedio',
+        ]);
+    }
+
+    public function indexAuditoriasTerminadasAction()
+    {
+        $auditorias = $this->getDoctrine()->getRepository('ChoferesBundle:Auditoria')
+            ->findByEstado(EstadoAuditoria::ID_TERMINADA);
+
+        return $this->render('ChoferesBundle:Auditoria:index.html.twig', [
+            'auditorias' => $auditorias,
+            'pagetitle' => 'Auditorias Terminadas',
+            'css_active' => 'auditoria_terminadas',
+        ]);
+    }
+
+    public function showAction($id)
+    {
+        $auditoria = $this->getDoctrine()->getRepository('ChoferesBundle:Auditoria')->find($id);
+
+        if (!$auditoria) {
+            throw $this->createNotFoundException('Unable to find Auditoria entity.');
+        }
+
+        return $this->render('ChoferesBundle:Auditoria:show.html.twig', [
+            'auditoria'  => $auditoria,
+            'css_active' => 'auditoria',
+        ]);
     }
 }
--- src/ChoferesBundle/Entity/Auditoria.php
+++ src/ChoferesBundle/Entity/Auditoria.php
@@ -2,6 +2,8 @@
 
 namespace ChoferesBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+
 /**
  * Auditoria
  */
@@ -13,6 +15,11 @@ class Auditoria
     private $id;
 
     /**
+     * @var \ChoferesBundle\Entity\CursoAuditoria
+     */
+    // private $cursos;
+
+    /**
      * @var \ChoferesBundle\Entity\Curso
      */
     private $curso;
@@ -21,6 +28,11 @@ class Auditoria
      * @var \ChoferesBundle\Entity\EstadoAuditoria
      */
     private $estado;
+
+    public function __construct()
+    {
+        // $this->cursos = new ArrayCollection();
+    }
 
     /**
      * Get id
@@ -33,8 +45,13 @@ class Auditoria
     }
 
     /**
-     * @return \ChoferesBundle\Entity\Curso
+     * @return \ChoferesBundle\Entity\CursoAuditoria
      */
+    // public function getCursos()
+    // {
+    //     return $this->cursos;
+    // }
+
     public function getCurso()
     {
         return $this->curso;
@@ -43,9 +60,11 @@ class Auditoria
     /**
      * @param \ChoferesBundle\Entity\Curso $curso
      */
-    public function setCurso($curso)
+    public function setCurso($curso = null)
     {
         $this->curso = $curso;
+
+        return $this;
     }
 
     /**
@@ -62,6 +81,8 @@ class Auditoria
     public function setEstado($estado)
     {
         $this->estado = $estado;
+
+        return $this;
     }
 
 }

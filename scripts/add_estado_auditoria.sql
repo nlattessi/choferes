85c8627cfaca42ba4d68ea103aa38244628a975a
Nahuel Lattessi <n_lattessi@hotmail.com>
2017-07-06 20:53:01 -0300

    agregado estado enviado al sql

--- scripts/add_estado_auditoria.sql
+++ scripts/add_estado_auditoria.sql
@@ -1,1 +1,1 @@
-INSERT INTO `choferes`.`estado_auditoria` (id, nombre) VALUES (1,'activa'), (2,'intermedio'), (3,'terminada'), (4,'borrador');
+INSERT INTO `choferes`.`estado_auditoria` (id, nombre) VALUES (1,'activa'), (2,'intermedio'), (3,'terminada'), (4,'borrador'), (5, 'enviada');

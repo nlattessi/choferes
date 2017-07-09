48dda60e13614b9ca41362083bf2bbdbe4f1eb2e
Nahuel Lattessi <n_lattessi@hotmail.com>
2017-07-08 21:54:57 -0300

    func

--- src/ChoferesBundle/Resources/public/assets/scripts/functions.js
+++ src/ChoferesBundle/Resources/public/assets/scripts/functions.js
@@ -1,15 +1,20 @@
-  function saveInLocalStorage(page)
-  {
-        localStorage.setItem(page, document.location.search);
-  }
+function saveInLocalStorage(page)
+{
+    localStorage.setItem(page, document.location.search);
+}
 
-  function getInLocalStorage(page)
-  {
-      var filtro =  localStorage.getItem(page);
+function getInLocalStorage(page)
+{
+    var filtro =  localStorage.getItem(page);
 
-      if ( ! (filtro != null && filtro != undefined) ) {
-          filtro = '';
-      }
+    if ( ! (filtro != null && filtro != undefined) ) {
+        filtro = '';
+    }
 
-      document.getElementById("listado").href = document.getElementById("listado").href +  filtro;
-  }
+    document.getElementById("listado").href = document.getElementById("listado").href +  filtro;
+}
+
+function stringIsEmpty(str)
+{
+    return (! str || str.length === 0);
+}
